b77d95b4ff1132a154b48b3e36846b1595b61b78
Ryan <ryan@rcorre.net>
2015-04-30 23:35:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+DTiled Allegro Example
+===
+
+This is an example of using the 
+[D Bindings](https://github.com/SiegeLord/DAllegro5) of 
+[Allegro5](http://sourceforge.net/p/alleg/allegro/ci/5.1/tree/) to render a
+[Tiled](mapeditor.org) map using [dtiled](https://github.com/rcorre/dtiled).
---
+++ resources/ground.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ground" tilewidth="32" tileheight="32">
+ <image source="../content/ground.png" width="320" height="320"/>
+ <terraintypes>
+  <terrain name="grass" tile="0"/>
+  <terrain name="water" tile="2"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="2" terrain="1,1,1,1"/>
+ <tile id="35" terrain="1,1,1,0"/>
+ <tile id="36" terrain="1,1,0,1"/>
+ <tile id="37" terrain="0,0,0,1"/>
+ <tile id="38" terrain="0,0,1,1"/>
+ <tile id="39" terrain="0,0,1,0"/>
+ <tile id="45" terrain="1,0,1,1"/>
+ <tile id="46" terrain="0,1,1,1"/>
+ <tile id="47" terrain="0,1,0,1"/>
+ <tile id="48" terrain="1,1,1,1"/>
+ <tile id="49" terrain="1,0,1,0"/>
+ <tile id="57" terrain="0,1,0,0"/>
+ <tile id="58" terrain="1,1,0,0"/>
+ <tile id="59" terrain="1,0,0,0"/>
+</tileset>
