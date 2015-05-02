5c42a2b9cd32e30b9aa98cbe2b78c6d9a04a37cb
Ryan <ryan@rcorre.net>
2015-05-01 23:14:09 -0400

    Draw a second tile layer.

--- src/app.d
+++ src/app.d
@@ -4,7 +4,6 @@ import std.stdio;
 
 import allegro5.allegro;
 import allegro5.allegro_image;
-import allegro5.allegro_font;
 import allegro5.allegro_ttf;
 import allegro5.allegro_color;
 import tiled;
@@ -13,35 +12,42 @@ int main(char[][] args)
 {
 	return al_run_allegro(
 	{
+    // basic allegro setup
 		al_init();
 
 		ALLEGRO_DISPLAY* display = al_create_display(800, 600);
-
 		ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60);
 
 		al_install_keyboard();
 		al_install_mouse();
 		al_init_image_addon();
-		al_init_font_addon();
 		al_init_ttf_addon();
 
 		al_register_event_source(queue, al_get_display_event_source(display));
 		al_register_event_source(queue, al_get_keyboard_event_source());
-		al_register_event_source(queue, al_get_mouse_event_source());
+		al_register_event_source(queue, al_get_timer_event_source(timer));
 
-    auto map = TiledMap.load("./content/map1.json");
+    // load the image we will use to draw tiles
 		ALLEGRO_BITMAP* tileAtlas = al_load_bitmap("./content/ground.png");
 
-    auto layer = map.getLayer("Ground");
-    auto tileset = map.getTileset("ground");
-		//ALLEGRO_FONT* font = al_load_font("DejaVuSans.ttf", 18, 0);
+    // load the map
+    auto map = TiledMap.load("./content/map1.json");
 
-		with(ALLEGRO_BLEND_MODE)
-		{
+    // the layers determine which tiles go where
+    auto groundLayer = map.getLayer("Ground");
+    auto featureLayer = map.getLayer("Features");
+
+    // the tileset contains data about the tiles
+    auto tileset = map.getTileset("ground");
+
+    with(ALLEGRO_BLEND_MODE) {
 			al_set_blender(ALLEGRO_BLEND_OPERATIONS.ALLEGRO_ADD, ALLEGRO_ALPHA, ALLEGRO_INVERSE_ALPHA);
 		}
 
+    al_start_timer(timer);
 		bool exit = false;
+		bool redraw = false;
 		while(!exit)
 		{
 			ALLEGRO_EVENT event;
@@ -72,14 +78,22 @@ int main(char[][] args)
 						exit = true;
 						break;
 					}
+          case ALLEGRO_EVENT_TIMER:
+          {
+            redraw = true;
+            break;
+          }
 					default:
 				}
 			}
 
-			al_clear_to_color(ALLEGRO_COLOR(0, 0, 0, 0));
-      map.drawLayer(layer, tileset, tileAtlas);
-			//al_draw_text(font, ALLEGRO_COLOR(1, 1, 1, 1), 70, 40, ALLEGRO_ALIGN_CENTRE, "Hello!");
-			al_flip_display();
+      if (redraw) {
+        redraw = false;
+        al_clear_to_color(ALLEGRO_COLOR(0, 0, 0, 0));
+        map.drawLayer(groundLayer, tileset, tileAtlas);
+        map.drawLayer(featureLayer, tileset, tileAtlas);
+        al_flip_display();
+      }
 		}
 
 		return 0;
@@ -88,9 +102,15 @@ int main(char[][] args)
 
 void drawLayer(TiledMap map, TiledLayer layer, TiledTileset tileset, ALLEGRO_BITMAP* atlas) {
   foreach(idx, gid ; layer.data) {
+    if (gid == 0) { continue; } // no tile at this spot
+
+    // based on the current index and the map grid size, we can determine where the top-left of the
+    // tile should be drawn.
     float dx = layer.idxToCol(idx) * map.tilewidth;
     float dy = layer.idxToRow(idx) * map.tileheight;
 
+    // the GID is used as an index into the tileset, and tells us what region of the tile atlas
+    // should be used to draw the tile.
     float sx = tileset.tileOffsetX(gid);
     float sy = tileset.tileOffsetY(gid);
     float sw = tileset.tilewidth;
